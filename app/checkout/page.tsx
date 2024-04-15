91cf48f6f5662e143f4d5f3ffd071443b0c4a917
Misas10 <manmisa2003@gmail.com>
2024-04-15 11:48:02 +0100

    forms working

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,23 +1,32 @@
 "use client";
 
 import '.././styles.css'
-import { useCallback } from 'react';
+import { ChangeEventHandler, useCallback, useState } from 'react';
 import React from 'react';
-// import FormElement from '../components/forms';
 import { redirect, useRouter } from 'next/navigation'
 import axios from 'axios';
-import { useForm, SubmitHandler, Path, UseFormRegister, ValidationRule, UseFormRegisterReturn } from "react-hook-form";
+import { useForm, Path, ValidationRule, UseFormRegisterReturn, FieldError, Merge, FieldErrorsImpl } from "react-hook-form";
+import { valibotResolver } from '@hookform/resolvers/valibot';
+
+import { minLength, string, object } from 'valibot';
+import * as v from 'valibot';
 
 
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/high-res.css'
 import Sucess from '../sucess/page';
-// import { RadioProps } from '@nextui-org/react';
-// import { on } from 'events';
-// import { register } from 'module';
-// import { finished } from 'stream';
 
 
+interface IFormValues {
+  'First Name': string
+  'Last Name': string
+  'Email Address': string
+  'Date of pickup': string
+  'Time of pickup': string
+  'Total Luggage': number
+  'Payment': string
+  'PhoneNumber': string
+}
 
 const PaymentPage = () => {
   var data;
@@ -26,29 +35,50 @@ const PaymentPage = () => {
   } else
     data = {}
 
-  interface IFormValues {
-    'First Name': string
-    'Last Name': string
-    'Email Address': string
-    'Date of pickup': string
-    'Time of pickup': string
-    'Total Luggage': number
-    'Payment': string
-    'PhoneNumber': string
-  }
+  const formSchema = object({
+    firstName: string([
+      minLength(1, 'Please enter your first name')
+    ]),
 
+    lastName: string([
+      minLength(1, 'Please enter your last name')
+    ]),
 
-  // const { register, handleSubmit } = useForm({ shouldUseNativeValidation: true });
+    email: string([
+      minLength(1, 'Please enter your email.'),
+      v.email('The email address is badly formatted.'),
+    ]),
+
+    // phoneNumber: string([
+    //   minLength(1, "Please enter the pickup Date")
+    // ]),
+
+    pickupDate: v.date("Please enter the pickup Date"),
+
+    pickupTime: string([
+      minLength(1, "Please enter the pickup Date")
+    ]),
+
+    payment: string([
+      minLength(1, "Please enter the pickup Date")
+    ]),
+
+    additionalInformation: v.optional(v.string()),
+
+    terms: v.literal(true, "Please accept our terms."),
+  });
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  type FormSchemaType = v.Output<typeof formSchema>
+
   const {
     register,
     handleSubmit,
-    setFocus,
     formState: { errors },
-  } = useForm<IFormValues>({ shouldUseNativeValidation: true })
-
-  React.useEffect(() => {
-    setFocus("First Name");
-  }, [setFocus])
+  } = useForm<FormSchemaType>({
+    resolver: valibotResolver(formSchema),
+  });
 
   const router = useRouter();
   const cancel = useCallback(() => {
@@ -59,55 +89,15 @@ const PaymentPage = () => {
   const destination = data.destination //searchParams.get("destination");
   // const searchParams = useSearchParams()
 
-  const firstName = register('First Name', { required: true, pattern: /^[A-Za-z]+$/i })
-  const lastName = register('Last Name', { required: true, pattern: /^[A-Za-z]+$/i })
-  const email = register('Email Address', { required: true })
-  const date = register("Date of pickup", { required: true })
-  const time = register("Time of pickup", { required: true })
 
-  // const [inputValue2, setInputValue2] = useState<string>('');
-  //
-  // const formInfo = {
-  //   "fistName": nameFirst,
-  //   "lastName": nameLast,
-  //   "email": email,
-  //   "pickupDate": date,
-  //   "pickupTime": time,
-  //   "totalLuggage": numberLuggage,
-  //   "paymentType": payment,
-  //   "additionalInfo": "",
-  //   "origin": origin,
-  //   "destination": destination,
-  // }
-
-  var options = [<option key={0} defaultValue={"--"} >--</option>];
+  const luggageOptions = [<option key={0} defaultValue={"--"} >--</option>];
   for (var i = 1; i <= 16; i++)
-    options.push(<option key={i}>{i}</option>);
-
-  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   // Do something with the input value stored in 'inputValue1'
-  //   e.preventDefault();
-  //   console.log('Input value 1:', nameFirst);
-  //   console.log('Input value 2:', nameLast);
-  //   console.log('Input value 3:', email);
-  //   console.log('Input value 4:', date);
-  //   console.log('Input value 5:', time);
-  //   console.log('Input value 6:', numberLuggage);
-  //   console.log('Input value 7:', payment);
-  //
-  //   if (checkIfIsNameValid()) {
-  //     localStorage.clear();
-  //     localStorage.setItem("formInfo", JSON.stringify(formInfo));
-  //     checkout();
-  //   }
-  //
-  // };
+    luggageOptions.push(<option key={i}>{i}</option>);
 
 
-  const onSubmit: SubmitHandler<IFormValues> = (data) => {
+  const onSubmit = (data: FormSchemaType) => {
     alert(JSON.stringify(data))
-    if (data.Payment !== "Pay online")
-      document.getElementById('my_modal_1').showModal();
+    console.log(data)
   }
 
   async function checkout(data: IFormValues) {
@@ -153,50 +143,50 @@ const PaymentPage = () => {
 
   type InputProps = {
     label: Path<IFormValues>;
-    register?: UseFormRegisterReturn;
+    register: UseFormRegisterReturn;
+    name: string;
     type: string;
     autoComplete?: string;
     placeholder?: string;
-    errorMessage?: string;
+    error?: FieldError | Merge<FieldError, FieldErrorsImpl<any>>;
     required?: boolean;
     inputValue?: string;
     showError?: boolean;
     pattern?: ValidationRule<RegExp>;
     className?: string;
+    value?: string;
+    onChange?: ChangeEventHandler<HTMLInputElement>;
   }
 
+  var _nameInputClassName = "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6";
 
-  var _nameInputClassName = "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ";
+  const Input = (props: InputProps) => {
+    return (
+      <>
+        <div className={"sm:col-span-4"}>
+          <label
+            className="block text-sm font-medium leading-6 text-gray-900">
+            {props.label}
+          </label>
 
-  // The following component is an example of your existing Input Component
-  const Input = ({ className, label, register, type, autoComplete, placeholder, errorMessage }: InputProps) => (
-    <>
-      <div className={"sm:col-span-4"}>
-        <label
-          className="block text-sm font-medium leading-6 text-gray-900">
-          {label}
-        </label>
+          <div className="mt-2">
+            <input
+              {...props.register}
+              className={_nameInputClassName}
+              name={props.name}
+              type={props.type}
+              autoComplete='given-name'
+            // onChange={props.onChange}
+            />
 
-        <div className="mt-2">
-          <input className={
-            className ?
-              className :
-              errors[label] ? _nameInputClassName + "ring-error" : _nameInputClassName
-          }
-            type={type}
-            placeholder={placeholder}
-            autoComplete={autoComplete}
-            {...register}
-            aria-invalid={errors[label] ? "true" : "false"}
-          />
-          {errors[label]?.type === "required" && (
-            <p role="alert" className='text-error'>{errorMessage}</p>
-          )}
+            {props.error && (<p>{props.error.message?.toString()}</p>)}
+
+          </div>
         </div>
-      </div>
 
-    </>
-  )
+      </>
+    )
+  }
 
   type RadioProps = {
     value: string
@@ -205,21 +195,64 @@ const PaymentPage = () => {
   const RadioInput = ({ value }: RadioProps) => (
     <>
       <input
+        {...register("payment")}
         className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
         type="radio"
         value={value}
-        // aria-invalid={errors["Payment"] ? "true" : "false"}
-        {...register("Payment", { required: true })}
+      // aria-invalid={errors["Payment"] ? "true" : "false"}
       />
     </>
   )
 
+  const personalInfoInputs = [
+    Input({
+      register: register('firstName'),
+      name: "firstName",
+      label: "First Name",
+      type: "text",
+      error: errors["firstName"],
+      // showError: errors.firstName ? true : false,
+    }),
 
+    Input({
+      register: register('lastName'),
+      name: "lastName",
+      label: "Last Name",
+      type: "text",
+      error: errors.lastName,
+    }),
 
+    Input({
+      register: register("email"),
+      name: "email",
+      label: "Email Address",
+      type: "email",
+      error: errors.email
+    })
+  ];
+
+  const tripDetailsInfo = [
+    Input({
+      register: register("pickupDate"),
+      label: "Date of pickup",
+      type: "date",
+      name: "pickupDate"
+      // onChange: (e) => setLastName(e.target.value)
+    }),
+
+    Input({
+      register: register("pickupTime"),
+      label: "Time of pickup",
+      type: "time",
+      name: "pickupTime"
+      // onChange: (e) => set(e.target.value)
+    })
+
+  ]
 
   return (
     <>
-      <div className=' m-3'>
+      <div className=' mx-48 my-16'>
         <h1 className=' font-semibold text-2xl'> Final steps</h1>
         <br />
         <p className=' border-b border-gray-900/10 pb-4'> We just need some more information to finish the reservation.</p>
@@ -229,30 +262,7 @@ const PaymentPage = () => {
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
-              <Input
-                type="text"
-                label='First Name'
-                autoComplete='given-name'
-                register={firstName}
-                errorMessage='Name is Invalid'
-              />
-
-              <Input
-                label='Last Name'
-                type='text'
-                autoComplete='family-name'
-                register={lastName}
-                errorMessage='Name is Invalid'
-              />
-
-
-              <Input
-                label='Email Address'
-                type='email'
-                autoComplete='email'
-                placeholder='example@gmail.com'
-                register={email}
-              />
+              {personalInfoInputs.map(input => input)}
 
               <div className="sm:col-span-4">
                 <label
@@ -261,18 +271,20 @@ const PaymentPage = () => {
                 </label>
 
                 <PhoneInput
+                  // {...register("")}
                   inputProps={{
-                    className: 'block w-full rounded-md border-0 px-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    className: 'block w-full rounded-md border-0 px-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                    required: true,
                   }}
                   country={'pt'}
-                  // onChange={setPhoneNumber}
+                  onChange={setPhoneNumber}
                   enableSearch={true}
                 />
               </div>
 
             </div>
-
           </div>
+
           <div className="border-b border-gray-900/10 pb-12">
             <h2 className="text-base font-semibold leading-7 text-gray-900">Trip details</h2>
             <br />
@@ -287,28 +299,12 @@ const PaymentPage = () => {
             </div>
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
-              <Input
-                // inputValue={date}
-                // onInputChange={(e) => setDate(e)}
-                // onSubmit={() => { }}
-                label='Date of pickup'
-                type='date'
-                register={date}
-              />
+              {tripDetailsInfo.map(input => input)}
 
-              <Input
-                // inputValue={time}
-                // onInputChange={(e) => setTime(e)}
-                // onSubmit={() => { }}
-                label='Time of pickup'
-                type='time'
-                register={time}
-              />
             </div>
           </div>
 
           <div className="border-b border-gray-900/10 pb-12">
-
             <h2 className="text-base font-semibold leading-7 text-gray-900">Luggage</h2>
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-3">
 
@@ -318,25 +314,8 @@ const PaymentPage = () => {
                   className="block text-sm font-medium leading-6 text-gray-900">Number of Suitcases</label>
                 <div className="mt-2">
                   <select className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-
-                    {...register("Total Luggage")}
                   >
-                    {options}
-                    {
-                      //
-                      // onChange={
-                      //   e => {
-                      //     var value = parseInt(e.target.value);
-                      //     if (isNaN(value)) {
-                      //       setNumberLuggage(0);
-                      //     }
-                      //     else {
-                      //       setNumberLuggage(Number(e.target.value));
-                      //     }
-                      //   }
-                      //
-                      // }
-                    }
+                    {luggageOptions}
                   </select>
                 </div>
               </div>
@@ -353,11 +332,11 @@ const PaymentPage = () => {
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                 </div>
               </div>
+
             </div>
           </div>
 
           <div className="border-b border-gray-900/10 pb-12">
-
             <h2 className="text-base font-semibold leading-7 text-gray-900">Payment</h2>
             <fieldset>
 
@@ -385,7 +364,7 @@ const PaymentPage = () => {
           <div className="relative flex gap-x-3">
             <div className="flex h-6 items-center">
               <input
-                required
+                {...register("terms")}
                 id="terms"
                 name="terms"
                 type="checkbox"
@@ -404,7 +383,6 @@ const PaymentPage = () => {
               Cancel
             </button>
             <button
-              // onClick={}
               type="submit"
               className="btn btn-success rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-whiteBg shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
@@ -415,10 +393,14 @@ const PaymentPage = () => {
         </form >
 
         <Sucess />
-      </div>
+      </div >
 
     </>
   )
 }
 
 export default PaymentPage;
+// function SafeParse(LoginSchema: v.ObjectSchema<{ firstName: v.StringSchema<string>; lastName: v.StringSchema<string>; email: v.StringSchema<string>; password: v.StringSchema<string>; }, undefined, { firstName: string; lastName: string; email: string; password: string; }>, arg1: { firstName: string; lastName: string; email: string; password: string; }) {
+//   throw new Error('Function not implemented.');
+// }
+
