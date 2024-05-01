fda85743fed38dc39d6d4a12a65e8c1f5e26b9e9
Misas10 <manmisa2003@gmail.com>
2024-05-01 22:35:50 +0100

    Some changes

--- app/api/route.jsx
+++ app/api/route.jsx
@@ -4,17 +4,17 @@ import { NextResponse, NextRequest } from "next/server";
 export async function POST(request) {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
     let data = await request.json();
-    let priceId = data.priceId
+    // let priceId = data.priceId
     const session = await stripe.checkout.sessions.create({
         line_items: [
             {
                 price_data: {
                     currency: "EUR",
-                    unit_amount: priceId * 100,
+                    unit_amount: data.price * 100,
                     product_data: {
                         name: "Trip",
                         description:
-                            "test dasds        ad asdasd",
+                           `From:${data.origin} - To:${data.destination} at ${data.timeOfPickup}`,
                     },
                 },
                 quantity: 1
